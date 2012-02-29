a20f30fe8a9330c6703bbf389e1c13b35f33a149
Naoyuki Ishida <39.ishida@gmail.com>
2012-02-29 12:18:31 +0900

    color syntax on

--- adminconsole.sh
+++ adminconsole.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+set -u
 
-echo "\n*****FileMaker Serverアドミンコンソール Mac版*****\n"
+echo "\n\e[1;34m*****FileMaker Serverアドミンコンソール Mac版*****\e[1;37m\n"
 
 # IDとパスワードを入力して変数に代入
 echo 'IDを入力してください...'
@@ -9,7 +10,7 @@ echo 'パスワードを入力してください...'
 read -s PW
 
 # ログインチェック
-fmsadmin -v -u ${ID} -p ${PW} || { echo 'IDとパスワードが違います。もっかいやり直してください。'; exit 1; }
+fmsadmin -v -u ${ID} -p ${PW} || { echo "\e[1;31mIDとパスワードが違います。もっかいやり直してください。\e[1;37m"; exit 1; }
 
 # 表示するメニューを配列に格納
 SELECT=(
@@ -56,7 +57,7 @@ while [ 0 ]; do
 # メニューをecho
 echo '======================================================================'
 for((i=0; i<${#SELECT[*]}; i++ )){
-  echo "$i: ${SELECT[i]}"
+  echo "\e[1;34m$i: ${SELECT[i]}\e[1;37m"
 }
 echo '======================================================================'
 echo "\n処理を選択してください..."
@@ -66,14 +67,14 @@ echo "\n処理を選択してください..."
 read CMD
 
 # 各コマンドのオプションを指定させて$EXECに最終的に実行するコマンドを代入
-case $CMD in
+case ${CMD} in
   0)
     exit;;
   1)
   # メッセージを送信する
     echo 'メッセージを入力してください...'
     read MSG
-    EXEC=${COMHEAD[$CMD]}" -m "$MSG
+    EXEC="${COMHEAD[$CMD]} -m ${MSG}"
     CAN=0
     ;;
   2)
@@ -85,17 +86,17 @@ case $CMD in
   # ファイルを閉じる
     echo "ファイルを指定して閉じる場合はIDで指定してください。\n指定がなければ全てのファイルを閉じます。\nIDはファイル一覧で表示されます..."
     read DBID
-    echo "ID:"${DBID}"のファイルを閉じます。よろしいですか？[y/n]..."
+    echo "ID:${DBID}のファイルを閉じます。よろしいですか？[y/n]..."
     read CAN
-    EXEC=${COMHEAD[$CMD]}' '${DBID}
+    EXEC="${COMHEAD[$CMD]} ${DBID}"
     ;;
   4)
   # ファイルを開く
     echo "ファイルを指定して開く場合はIDで指定してください。\n指定がなければ全てのファイルを開きます。\nIDはファイル一覧で表示されます..."
     read DBID
-    echo "ID:"${DBID}"のファイルを開きます。よろしいですか？[y/n]..."
+    echo "ID:${DBID}のファイルを開きます。よろしいですか？[y/n]..."
     read CAN
-    EXEC=${COMHEAD[$CMD]}' '${DBID}
+    EXEC="${COMHEAD[$CMD]} ${DBID}"
     ;;
   5)
   # スケジュール一覧を表示する
@@ -106,31 +107,31 @@ case $CMD in
   # スケジュールを実行する
     echo "実行するスケジュールのIDを指定してください。\nIDはスケジュール一覧で表示されます..."
     read SCHEID
-    echo "ID:"${SCHEID}"のスケジュールを実行します。よろしいですか？[y/n]..."
+    echo "ID:${SCHEID}のスケジュールを実行します。よろしいですか？[y/n]..."
     read CAN
-    EXEC=${COMHEAD[$CMD]}' '${SCHEID}
+    EXEC="${COMHEAD[$CMD]} ${SCHEID}"
     ;;
   7)
   # スケジュールを有効にする
     echo "有効にするスケジュールのIDを指定してください。\nIDはスケジュール一覧で表示されます..."
     read SCHEID
-    echo "ID:"${SCHEID}"のスケジュールを有効にします。よろしいですか？[y/n]"
+    echo "ID:${SCHEID}のスケジュールを有効にします。よろしいですか？[y/n]"
     read CAN
-    EXEC=${COMHEAD[$CMD]}' '${SCHEID}
+    EXEC="${COMHEAD[$CMD]} ${SCHEID}"
     ;;
   8)
   # スケジュールを無効にする
     echo "無効にするスケジュールのIDを指定してください。\nIDはスケジュール一覧で表示されます..."
     read SCHEID
-    echo "ID:"${SCHEID}"のスケジュールを無効にします。よろしいですか？[y/n]"
+    echo "ID:${SCHEID}のスケジュールを無効にします。よろしいですか？[y/n]"
     read CAN
-    EXEC=${COMHEAD[$CMD]}' '${SCHEID}
+    EXEC="${COMHEAD[$CMD]} ${SCHEID}"
     ;;
   9)
   # データベースのバックアップ
     echo "バックアップするファイルのIDを指定してください。\n指定がなければ全てのファイルをバックアップします..."
     read DBID
-    EXEC=${COMHEAD[$CMD]}' '${DBID}
+    EXEC="${COMHEAD[$CMD]} ${DBID}"
     CAN=0
     ;;
   10)
@@ -172,7 +173,7 @@ case $CMD in
 esac
 
 # $CANの設定
-case $CAN in
+case ${CAN} in
   "y")
     CAN=0;;
   "n")
@@ -181,9 +182,14 @@ case $CAN in
     echo 'どっちも選ばない、はナシです。'
 esac
 
-if [ $CAN -eq 0 ]; then
+if [ ${CAN} -eq 0 ]; then
   # fmsadminコマンドを実行
-  fmsadmin $EXEC -y -u $ID -p $PW
+  fmsadmin ${EXEC} -y -u ${ID} -p ${PW}
+  if [ $? == 0 ]; then
+    echo "\n\e[1;32m処理が正常に終了しました。\e[1;37m\n"
+  else
+    echo "\n\e[1;31m処理が失敗しました。\n\e[1;37m"
+  fi
 fi
 
 done
